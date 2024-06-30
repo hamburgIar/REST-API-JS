67d0eae7dc81139766573e79e2f0017bf4bd9d78
Hamburglar <92908058+hamburgIar@users.noreply.github.com>
2024-07-01 00:56:52 +0300

    update client

--- client/main.js
+++ client/main.js
@@ -1,19 +1,18 @@
 const getInfoBtn = document.getElementById("getInfo")
+const createUserBtn = document.getElementById("createUser")
 const infoContent = document.getElementById("infoContent")
 
-const beckURL = "http://localhost:8080"
+const beckURL = "http://localhost:8080/api/v1"
 
-async function getUsers() {
+async function fetchAndGetData(url, options) {
     try {
-        const response = await fetch(`${beckURL}/api/v1/users/`)
+        const response = await fetch(url, options)
 
         if (!response.ok) {
             throw new Error(`Ошибка: ${response.status}`);
         }
 
-        const data = await response.json()
-
-        return data.users
+        return await response.json()
     }
     catch (error) {
         console.log(error)
@@ -21,7 +20,27 @@ async function getUsers() {
 }
 
 getInfoBtn.addEventListener("click", async () => {
-    const data = await getUsers()
+    const data = await fetchAndGetData(`${beckURL}/users/`, {})
 
-    infoContent.innerText = JSON.stringify(...data)
+    infoContent.innerText = JSON.stringify(data.users)
+})
+
+createUserBtn.addEventListener("click", async (event) => {
+    event.preventDefault()
+
+    const user = {
+        firstName: "Ivan",
+        lastName: "Petrov",
+        age: 20
+    }
+        
+    const data = await fetchAndGetData(`${beckURL}/users/create`, {
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8'
+        },
+        method: 'POST',
+        body: JSON.stringify(user)
+    })
+
+    console.log(data)
 })
